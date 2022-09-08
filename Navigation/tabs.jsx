74c747f901ea387c13c98b650ac1d1b676a9992f
Zain Asghar <zainasghar200@gmail.com>
2022-09-08 22:37:52 +0500

    reverted changes

--- Navigation/tabs.jsx
+++ Navigation/tabs.jsx
@@ -1,49 +1,10 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screens/HomeScreen';
 import MoreScreen from '../Screens/MoreScreen';
 import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
 const Tab = createBottomTabNavigator();
 
 function Tabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName="HomeScreen"
-      screenOptions={{
-        tabBarActiveTintColor: '#e91e63',
-      }}>
-      <Tab.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({focused}) => (
-            <Image
-              source={require('../assets/icons/home.png')}
-              style={{
-                width: 25,
-                height: 25,
-                tintColor: focused ? 'Red' : 'Green',
-              }}></Image>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="MoreScreen"
-        component={MoreScreen}
-        options={{
-          tabBarLabel: 'Updates',
-          tabBarIcon: ({color, size}) => (
-            <Image
-              source={require('../assets/icons/menu.png')}
-              style={{
-                width: 25,
-                height: 25,
-              }}></Image>
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
+  return <Text>Tabs</Text>;
 }
 
 export default Tabs;
--- android/app/src/main/java/com/rn_assessment_ts/MainActivity.java
+++ android/app/src/main/java/com/rn_assessment_ts/MainActivity.java
@@ -3,7 +3,6 @@ package com.rn_assessment_ts;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import android.os.Bundle;
 public class MainActivity extends ReactActivity {
 
   /**
@@ -14,10 +13,6 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "RN_Assessment_TS";
   }
-   @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(null);
-    }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
