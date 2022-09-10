03dd93039099f7c1a3187b2a827f6390c15e1cec
Zain Asghar <zainasghar200@gmail.com>
2022-09-10 17:51:14 +0500

    css

--- Screens/AddScreen.tsx
+++ Screens/AddScreen.tsx
@@ -76,6 +76,7 @@ const styles = StyleSheet.create({
     marginTop: 30,
   },
   input: {
+    textAlignVertical: 'top',
     paddingHorizontal: 10,
     backgroundColor: 'white',
     borderRadius: 7,
